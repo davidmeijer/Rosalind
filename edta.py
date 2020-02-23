3f0802addbc23f0ccb74531e83718f5438eba737
David Meijer <davidmeijer@Davids-iMac.bronland.duwo.lan>
2020-02-23 21:11:32 +0100

    finished EDTA assignment

---
+++ edta.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+"""Author: David Meijer"""
+
+import argparse
+
+def DefineArguments():
+    """Defines possible command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', type = str, required = True,
+                        help = 'Rosalind exercise input file.')
+
+    return parser
+
+def FastaParser(fn):
+    """Parser FASTA file into dictionary."""
+    fasta = {}
+
+    with open(fn, 'r') as fo:
+        for line in [line.strip() for line in fo]:
+            if line.startswith('>'):
+                header = line
+                fasta[header] = ''
+            else:
+                fasta[header] += line
+
+    return fasta
+
+class Matrix(object):
+    """Matrix class."""
+    def __init__(self, nrows = 0, ncols = 0, fill = 0):
+        self._nrows = nrows
+        self._ncols = ncols
+        self._fill = fill
+
+        self.SetMatrix()
+
+    def __str__(self):
+        rows = []
+
+        for row in self.matrix:
+            rows.append(str(' '.join([str(item) for item in row])))
+
+        return '\n'.join(rows)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __hash__(self):
+        return hash(self.matrix)
+
+    @property
+    def nrows(self):
+        return self._nrows
+
+    @property
+    def ncols(self):
+        return self._ncols
+
+    @property
+    def fill(self):
+        return self._fill
+
+    @nrows.setter
+    def nrows(self, new_nrows):
+        self._nrows = new_nrows
+        self.SetMatrix()
+
+    @ncols.setter
+    def ncols(self, new_ncols):
+        self._ncols = new_ncols
+        self.SetMatrix()
+
+    @ncols.setter
+    def fill(self, new_fill):
+        self._fill = new_fill
+        self.SetMatrix()
+
+    def SetMatrix(self):
+        """Sets matrix with defined values."""
+        self.matrix = [[self._fill for i in range(self.ncols)] \
+                        for j in range(self.nrows)]
+
+    def SetValue(self, x, y, value):
+        """Sets value in matrix at intersection of row x and column y."""
+        self.matrix[x][y] = value
+        return self.matrix
+
+    def ReturnValue(self, x, y):
+        """Returns value in matrix at intersection of row x and column y."""
+        return self.matrix[x][y]
+
+    def Dimensions(self):
+        """Return matrix dimensions."""
+        return [self.ncols, self.nrows]
+
+    def SetDiagonal(self, value):
+        """Sets diagonal of matrix to certain value."""
+
+    def Transpose(self):
+        """Return transposed matrix."""
+
+    def MultiplyMatrix(self, other):
+        """Returns product of matrices."""
+
+    def SubtractMatrix(self, other):
+        """Returns subtraction of matrices."""
+
+    def AddMatrix(self, other):
+        """Returns addition of matrices."""
+
+
+class Protein(object):
+    """Protein class."""
+    def __init__(self, string):
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+    def __repr__(self):
+        return self.string
+
+    def __eq__(self, other):
+        return self.string == other.string
+
+    def __hash__(self):
+        return hash(self.string)
+
+    def Length(self):
+        """Returns number of amino acids in peptide string."""
+        return len(self.string)
+
+    def LevenshteinDistanceMatrix(self, other):
+        """Constructs Levenshtein distance matrix between two peptides."""
+        dist = Matrix(self.Length() + 1, other.Length() + 1)
+
+        for i in range(0, self.Length() + 1):
+            dist.SetValue(i, 0, i)
+
+        for j in range(0, other.Length() + 1):
+            dist.SetValue(0, j, j)
+
+        for j in range(1, other.Length() + 1):
+            for i in range(1, self.Length() + 1):
+                if self.string[i - 1] == other.string[j - 1]:
+                    substitutionCost = 0
+                else:
+                    substitutionCost = 1
+
+                dist.SetValue(i, j, min([
+                dist.ReturnValue(i - 1, j) + 1,
+                dist.ReturnValue(i, j - 1) + 1,
+                dist.ReturnValue(i - 1, j - 1) + substitutionCost
+                ]))
+
+        return dist
+
+    def LevenshteinDistance(self, other):
+        """Returns Levenshtein distance between two peptides."""
+        dist = self.LevenshteinDistanceMatrix(other)
+        return dist.ReturnValue(self.Length(), other.Length())
+
+    def LevenshteinAlignment(self, other):
+        """Returns Levenshtrein alignment between two peptides."""
+        dist = self.LevenshteinDistanceMatrix(other)
+        return self.LevenshteinTraceback(other, dist,
+                                         self.Length(), other.Length())
+
+    def LevenshteinTraceback(self, other, dist, i, j):
+        """Traceback function for Levenshtrein alignment."""
+        if i == 0 or j == 0:
+            return ''
+
+        if self.string[i - 1] == other.string[j - 1]:
+            return self.LevenshteinTraceback(other, dist, i - 1, j - 1) + \
+            self.string[i - 1]
+
+        if dist.ReturnValue(i - 1, j - 1) < min([
+        dist.ReturnValue(i - 1, j),
+        dist.ReturnValue(i, j - 1)
+        ]):
+            return self.LevenshteinTraceback(other, dist, i - 1, j - 1) + \
+            self.string[i - 1]
+
+        if dist.ReturnValue(i, j - 1) < dist.ReturnValue(i - 1, j):
+            return self.LevenshteinTraceback(other, dist, i, j - 1) + \
+            '-'
+        else:
+            return self.LevenshteinTraceback(other, dist, i - 1, j) + \
+            self.string[i - 1]
+
+    def CommonLongestSubstring(self, other):
+        """Calculates common longest substring between peptides."""
+
+def main():
+    args = DefineArguments().parse_args()
+    fasta = FastaParser(args.i)
+
+    s = Protein(fasta[list(fasta.keys())[0]]) # Ugh...
+    t = Protein(fasta[list(fasta.keys())[1]]) # Ugh...
+
+    with open('output_edta.txt', 'w') as fo:
+        fo.write('{0}\n{1}\n{2}'.format(
+        s.LevenshteinDistance(t),
+        s.LevenshteinAlignment(t),
+        t.LevenshteinAlignment(s)
+        ))
+
+if __name__ == '__main__':
+    main()
--- lcsq.py
+++ lcsq.py
@@ -23,11 +23,11 @@ def parse_fasta(fn):
 
     Args:
         fn (str): file name of Rosalind input file.
-        
+
     Returns:
         fasta_dict (dict): contains sequenses as {header:sequence,...}."""
     fasta_dict = {}
-    
+
     with open(fn) as fo:
         for line in [line.strip() for line in fo]:
             if line.startswith('>'):
@@ -39,35 +39,35 @@ def parse_fasta(fn):
     return fasta_dict
 
 def random_string(slen, char_set='ACTG'):
-    """Generate a random string of fixed length. 
-    
+    """Generate a random string of fixed length.
+
     Default: DNA.
-    
+
     Args:
         slen (int): length of random string.
         char_set (str): char to choose from for random string.
-        
+
     Returns:
         rstring (str): random string of length slen."""
     rstring = ''.join(random.choice(char_set) for i in range(slen))
-    
+
     return rstring
-    
+
 def compute_lcs(A, B):
     """Computes Longest Common Subsequences between strings A and B.
-    
+
     Args:
         A (str): string A.
         B (str): string B.
-        
+
     Returns:
         X (arr): matrix containing LCS paths for strings A and B."""
     lenA, lenB = len(A), len(B)
-    
+
     # Create matrix of zeros with dimensions lenA (col), lenB (row):
-    X = [[0 for i in range(lenA + 1)] for j in range(lenB + 1)] 
+    X = [[0 for i in range(lenA + 1)] for j in range(lenB + 1)]
     # lenA/lenB + 1 is for inserting a zero column and row!
-    
+
     # Populate X with scores for calculating LCS:
     for i in range(lenA):
         for j in range(lenB):
@@ -77,47 +77,47 @@ def compute_lcs(A, B):
             else:
                 # If not the same, get highest value from top/left:
                 X[j + 1][i + 1] = max(X[j + 1][i], X[j][i + 1])
-                
+
     return X
 
 def backtrack_lcs(matrix, A, B, i, j):
     """Reads out all routes (LCSs) from LCS matrix.
-    
+
     Args:
         matrix (list of lists): containing LCSs.
         A (str): string A.
         B (str): string B.
         i (int): location in A.
         j (int): location in B.
-    
+
     Returns:
         lcs (str): longest common substring from matrix."""
     # Return LCS when begin of one of the strings is reached:
     if i == 0 or j == 0:
         return ""
-    
+
     # If equal return char:
     if A[i - 1] ==  B[j - 1]:
         return backtrack_lcs(matrix, A, B, i - 1, j - 1) + A[i - 1]
-        
+
     # Choose left over top if larger, otherwise (equal) choose top:
     if matrix[j][i - 1] > matrix[j - 1][i]:
         return backtrack_lcs(matrix, A, B, i - 1, j)
-    
+
     return backtrack_lcs(matrix, A, B, i, j - 1)
 
 def main():
     """Main code."""
     args = define_arguments().parse_args()
     fasta = parse_fasta(args.input)
-    
+
     # Get the two sequences from the FASTA dictionary:
     seq1 = fasta[[key for key in fasta.keys()][0]]
     seq2 = fasta[[key for key in fasta.keys()][1]]
-    
+
     # Compute LCS matrix for two strings:
     LCS_matrix = compute_lcs(seq1, seq2)
-    
+
     # Get max LCS length from matrix:
     #print(max([max(x) for x in LCS_matrix]))
 
